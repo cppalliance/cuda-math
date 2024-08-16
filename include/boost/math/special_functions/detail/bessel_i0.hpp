7efe28c8339826b3144a094ef19347892dd3597a
Matt Borland <matt@mattborland.com>
2024-08-16 11:09:09 -0400

    Add GPU support to bessel_i1

--- include/boost/math/special_functions/detail/bessel_i0.hpp
+++ include/boost/math/special_functions/detail/bessel_i0.hpp
@@ -1,5 +1,6 @@
 //  Copyright (c) 2006 Xiaogang Zhang
 //  Copyright (c) 2017 John Maddock
+//  Copyright (c) 2024 Matt Borland
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
