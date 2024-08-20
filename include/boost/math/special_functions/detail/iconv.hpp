65f68411685baf8f39f4f4890af8c3fbc1aba7d5
Matt Borland <matt@mattborland.com>
2024-08-20 13:17:59 -0400

    Markup iconv

--- include/boost/math/special_functions/detail/iconv.hpp
+++ include/boost/math/special_functions/detail/iconv.hpp
@@ -10,26 +10,27 @@
 #pragma once
 #endif
 
+#include <boost/math/tools/config.hpp>
 #include <boost/math/tools/type_traits.hpp>
 #include <boost/math/special_functions/round.hpp>
 
 namespace boost { namespace math { namespace detail{
 
 template <class T, class Policy>
-inline int iconv_imp(T v, Policy const&, boost::math::true_type const&)
+BOOST_MATH_GPU_ENABLED inline int iconv_imp(T v, Policy const&, boost::math::true_type const&)
 {
    return static_cast<int>(v);
 }
 
 template <class T, class Policy>
-inline int iconv_imp(T v, Policy const& pol, boost::math::false_type const&)
+BOOST_MATH_GPU_ENABLED inline int iconv_imp(T v, Policy const& pol, boost::math::false_type const&)
 {
    BOOST_MATH_STD_USING
    return iround(v, pol);
 }
 
 template <class T, class Policy>
-inline int iconv(T v, Policy const& pol)
+BOOST_MATH_GPU_ENABLED inline int iconv(T v, Policy const& pol)
 {
    typedef typename boost::math::is_convertible<T, int>::type tag_type;
    return iconv_imp(v, pol, tag_type());
