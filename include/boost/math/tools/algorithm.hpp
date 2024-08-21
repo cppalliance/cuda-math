7429375132fd0df86d51dbf3a0f3d81594243b41
Matt Borland <matt@mattborland.com>
2024-08-21 14:36:19 -0400

    Fix missing marker

--- include/boost/math/tools/algorithm.hpp
+++ include/boost/math/tools/algorithm.hpp
@@ -37,7 +37,7 @@ BOOST_MATH_GPU_ENABLED void fill(ForwardIt first, ForwardIt last, const T& value
 }
 
 template <typename OutputIt, typename Size, typename T>
-OutputIt fill_n(OutputIt first, Size count, const T& value)
+BOOST_MATH_GPU_ENABLED OutputIt fill_n(OutputIt first, Size count, const T& value)
 {
     for (Size i = 0; i < count; i++)
     {
